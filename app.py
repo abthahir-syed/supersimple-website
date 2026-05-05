e56562f95c263f044b30e2dc5717643935c4b0ab
Abthahir <yourmail@gmail.com>
2026-05-05 18:10:14 +0530

    fixed final render code

--- app.py
+++ app.py
@@ -2265,9 +2265,6 @@ if __name__ == "__main__":
 
             time.sleep(60)
 
-    import threading
-    threading.Thread(target=run_auto_reassign, daemon=True).start()
-
     # ✅ IMPORTANT CHANGE
     app.run(host="0.0.0.0", port=5000)
 
